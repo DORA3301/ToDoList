0d0b132f8652c56c37840a6901e66090c410d10e
DORA3301 <101839244+DORA3301@users.noreply.github.com>
2023-01-19 20:56:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.post("/", function(req, res){
   res.redirect("/");
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000");
 });
